8e01d13429444179d54dc64d22d7ab142392e9d0
Kwola AI <info.kwolalabs@gmail.com>
2025-04-04 22:30:11 +0200

    Update database.py

--- db/database.py
+++ db/database.py
@@ -2,11 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db/sparrows.db"  # Update this with your actual database URL
+# Replace these with your MySQL credentials, database info, and port
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://2p2jGBAmdebWJcX.root:y9DuejnV7qsCaZBs@gateway01.us-west-2.prod.aws.tidbcloud.com:4000/ssa_db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# Create the engine with the new MySQL URL
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Set up sessionmaker with no need for check_same_thread (SQLite-specific)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
