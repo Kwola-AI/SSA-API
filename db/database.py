be82ba0c432aee4988eb293920109af8bd8d9d6c
Kwola AI <info.kwolalabs@gmail.com>
2025-04-05 09:49:42 +0200

    Update database.py

--- db/database.py
+++ db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres.lktwyssqcxwrajcwikvd:asdf123@aws-0-us-west-1.pooler.supabase.com:5432/ssa_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.lktwyssqcxwrajcwikvd:asdf123@aws-0-us-west-1.pooler.supabase.com:6543/postgres?pgbouncer=true"
 
 # Create engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
