e3b3610c9ab530b75d2fc71a4ff55df1dfd5238a
Kwola AI <info.kwolalabs@gmail.com>
2025-04-05 09:31:33 +0200

    Update database.py

--- db/database.py
+++ db/database.py
@@ -3,12 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # ✅ PostgreSQL connection via psycopg2
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql+psycopg2://kwola:j2sGzh9wBZoehJrkBx1lqDgeRFgjZQdf@dpg-cvocombuibrs73boa3m0-a:5432/ssa_db"
-)
+DIRECT_URL="postgresql://postgres.lktwyssqcxwrajcwikvd:[asdf123]@aws-0-us-west-1.pooler.supabase.com:5432/ssa_db"
+
+
 
 # Create engine
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DIRECT_URL)
 
 # Create session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
